c5662a5946cb5345e66da16309553d50676933b6
Akmal Rakhimov <arakhimoff@gmail.com>
2016-01-11 06:22:51 +0900

    Coded minimum number in a queue

--- src/stackqueue/Queue.java
+++ src/stackqueue/Queue.java
@@ -3,22 +3,65 @@ package stackqueue;
 import linkedlists.Node;
 
 public class Queue {
-	Node top;
+	private Node first, last;
+	private int min = Integer.MAX_VALUE;
 	
 	public void enqueue(int n) {
-		Node t = new Node(n);
-		
-		Node curr = top;
-		while(curr != null) {
-			curr = curr.next;
+		if(n < min) {
+			min = n;
 		}
-		
-		curr = t;
+		if(first == null) {
+			last = new Node(n);
+			first = last;
+		} else {
+			last.next = new Node(n);
+			last = last.next;
+		}
 	}
 	
-	public int dequeue() {
-		int res = top.val;
-		top = top.next;
-		return res;
+	public Integer dequeue() {
+		
+		if(first != null) {
+			int res = first.val;
+			first = first.next;
+			
+			if(res == min) {
+				min = first.val;
+				Node curr = first;
+				while(curr != null) {
+					if(curr.val < min) {
+						min = curr.val;
+					}
+					curr = curr.next;
+				}
+			}
+			
+			return res;
+		}
+		
+		return null;
 	}
+	
+	public int min() {
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		Queue q = new Queue();
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(3);
+		
+		System.out.println(q.min());
+		q.dequeue();
+		System.out.println(q.min());
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Queue [first=" + first + ", last=" + last + ", min=" + min + "]";
+	}
+	
+	
 }
